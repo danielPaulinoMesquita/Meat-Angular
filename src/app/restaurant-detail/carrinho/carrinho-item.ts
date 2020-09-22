16e96354f0ecf989d4cc44b60f5f106a259677de
danielPaulinoMesquita <daniel-paulino@hotmail.com>
2020-09-22 01:56:36 -0300

    30- Animações com KeyFrames nos itens do carrinho.

--- src/app/restaurant-detail/carrinho/carrinho-item.ts
+++ src/app/restaurant-detail/carrinho/carrinho-item.ts
@@ -1,10 +1,11 @@
 import { MenuItem } from "../menu-item/menu-item.model";
 
 export class CarrinhoItem{
-    constructor(public menuItem: MenuItem, 
-        public quantity:number=1){}
+    constructor(
+        public menuItem: MenuItem,
+        public quantity: number = 1) {}
 
-    value():number{
+    value(): number {
         return this.menuItem.price * this.quantity;
     }
 }
--- src/app/restaurant-detail/carrinho/carrinho.component.ts
+++ src/app/restaurant-detail/carrinho/carrinho.component.ts
@@ -1,18 +1,36 @@
 import { Component, OnInit } from '@angular/core';
 import { CarrinhoService } from './carrinho.service';
+import {trigger, state, style, transition, animate, keyframes} from '@angular/animations';
 
 @Component({
   selector: 'mt-carrinho',
-  templateUrl: './carrinho.component.html'
+  templateUrl: './carrinho.component.html',
+  animations: [
+    trigger('row', [
+      state('ready', style({ opacity: 1})),
+      transition('void => ready', animate('300ms 0s ease-in', keyframes([
+        style({opacity: 0, transform: 'translateX(-30px)', offset: 0}),
+        style({opacity: 0.8, transform: 'translateX(10px)', offset: 0.8}),
+        style({opacity: 1, transform: 'translateX(0px)', offset: 1})
+      ]))),
+      transition('ready => void', animate('500ms 0s ease-out', keyframes([
+        style({opacity: 1, transform: 'translateX(0px)', offset: 0}),
+        style({opacity: 0.8, transform: 'translateX(-10px)', offset: 0.2}),
+        style({opacity: 0, transform: 'translateX(30px)', offset: 1})
+      ])))
+    ])
+  ]
 })
 export class CarrinhoComponent implements OnInit {
+
+  rowState = 'ready';
 
   constructor(private carrinhoService: CarrinhoService) { }
 
   ngOnInit() {
   }
 
-  items():any[]{
+  items(): any [] {
     return this.carrinhoService.items;
   }
 
@@ -20,15 +38,15 @@ export class CarrinhoComponent implements OnInit {
     return this.carrinhoService.total();
   }
 
-  clear(){
+  clear() {
     this.carrinhoService.clear();
   }
 
-  removeItem(item:any){
+  removeItem(item: any) {
     this.carrinhoService.removeItem(item);
   }
 
-  addItem(item:any){
+  addItem(item: any) {
     this.carrinhoService.addItem(item);
   }
 
