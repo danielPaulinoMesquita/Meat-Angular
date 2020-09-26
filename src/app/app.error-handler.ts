3bc78822bd56e90a7e57a3ea6d0c8a68ae5f5528
danielPaulinoMesquita <daniel-paulino@hotmail.com>
2020-09-26 17:34:42 -0300

    38- Novo Módulo HttpClient Refactoring da Aplicação.

--- src/app/app.error-handler.ts
+++ src/app/app.error-handler.ts
@@ -1,17 +1,18 @@
-import {Response} from '@angular/http';
+import {HttpErrorResponse} from '@angular/common/http';
 import { Observable } from 'rxjs/Observable';
 
-export class ErrorHandler{
-    static handleError(error:Response | any){
-        let errorMessage:string;
+export class ErrorHandler {
+  static handleError(error: HttpErrorResponse | any){
+    let errorMessage: string;
 
-        if(error instanceof Response){
-            errorMessage= `Erro ${error.status} ao acessar a URL ${error.url} - ${error.statusText}`
-        }else{
-            errorMessage=error.toString();
-        }
-        console.log(errorMessage);
-        return Observable.throw(errorMessage);
-    };
-    
+    if (error instanceof HttpErrorResponse){
+      const body = error.error;
+      errorMessage = `Erro ${error.status} ao acessar a URL ${error.url} - ${error.statusText || ''} ${body}`
+    }else {
+      errorMessage = error.toString();
+    }
+    console.log(errorMessage);
+    return Observable.throw(errorMessage);
+  };
+
 }
--- src/app/order/order.service.ts
+++ src/app/order/order.service.ts
@@ -3,12 +3,12 @@ import { CarrinhoService } from "app/restaurant-detail/carrinho/carrinho.service
 import { CarrinhoItem } from "app/restaurant-detail/carrinho/carrinho-item";
 import {Order} from './order.model';
 import {Observable} from 'rxjs';
-import {Headers, Http, RequestOptions} from '@angular/http';
+import {HttpClient} from '@angular/common/http';
 import {MEAT_API} from '../app-api';
 
 @Injectable()
 export class OrderService{
-    constructor(private carrinhoService: CarrinhoService, private http: Http) {
+    constructor(private carrinhoService: CarrinhoService, private http: HttpClient) {
 
     }
 
@@ -37,13 +37,7 @@ export class OrderService{
     }
 
   checkOrder(order: Order): Observable<string> {
-      const headers = new Headers();
-      headers.append('Content-Type', 'application/json');
-
-      return this.http.post(`${MEAT_API}/orders`,
-        JSON.stringify(order),
-        new RequestOptions({headers: headers}))
-        .map(response => response.json())
-        .map(order => order.id);
+     return this.http.post<Order>(`${MEAT_API}/orders`, order)
+       .map( order => order.id);
   }
 }
