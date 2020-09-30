41621654b521f865329450e32becb23621da168e
danielPaulinoMesquita <daniel-paulino@hotmail.com>
2020-09-30 02:21:14 -0300

    43- Middleware de autorização (Vericando token recebido)

--- backend/server.ts
+++ backend/server.ts
@@ -5,6 +5,7 @@ import * as fs from 'fs';
 import * as https from 'https';
 
 import {handleAutentication} from './auth';
+import {handleAuthorization} from './authz';
 
 const server: Express = jsonServer.create()
 const router = jsonServer.router('db.json')
@@ -19,6 +20,7 @@ server.use(jsonServer.bodyParser)
 
 // middleware para  login
 server.post('/login', handleAutentication)
+server.use('/orders', handleAuthorization)
 
 // Use default router
 server.use(router)
