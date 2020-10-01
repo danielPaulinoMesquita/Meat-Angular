8612016b9ea80104a430445954dd263abe75e1e6
danielPaulinoMesquita <daniel-paulino@hotmail.com>
2020-10-01 00:00:26 -0300

    44- Preparando a página de Login

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,10 +6,12 @@ import { MenuComponent } from './restaurant-detail/menu/menu.component';
 import { ReviewsComponent } from './reviews/reviews.component';
 import {OrderSummaryComponent} from './order-summary/order-summary.component';
 import {NotFoundComponent} from './not-found/not-found.component';
+import {LoginComponent} from './security/login/login.component';
 
 export const ROUTES: Routes = [
     { path: '', component: HomeComponent },
-    { path: 'about', loadChildren: './about/about.module#AboutModule' },
+  { path: 'login', component: LoginComponent},
+  { path: 'about', loadChildren: './about/about.module#AboutModule' },
     { path: 'restaurant', component: RestaurantsComponent },
     { path: 'restaurant-detail/:id', component: RestaurantDetailComponent,
         children: [
